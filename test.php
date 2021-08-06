ffb0f7636ad44bb9626ae422d388678152a5932d
SakhruAdrian <88219085+SakhruAdrian@users.noreply.github.com>
2021-08-06 09:46:19 +0700

    test.php
    ujicoba

---
+++ test.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+	<?php
+		echo " Nama saya : Sakhru Adrian S <br> " ;
+		echo " Kelas : XI RPL 1 <br> " ;
+		echo " Sekolah : SMK Mahardhika <br> " ;
+		echo " Cita-Cita : menjadi Astronot <br> " ;
+		
+	?>
+</body>
+</html>
